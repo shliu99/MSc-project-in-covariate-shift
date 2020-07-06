46ef25bbb5392fd6fd33d527e40e200c0f23b71c
Shuhan Liu <shuhan.liu.99@gmail.com>
2020-07-06 19:38:28 +0800

    change file name

--- generate_phenotype.R
+++ generate_phenotype.R
@@ -30,30 +30,39 @@ sample_beta <- function(len, alpha, seed = 2000){
 # upper: the maximal threshold for the porportion of positive phenotype in Afr
 # seed - set seeds
 
-generate_phenotype <- function(eur,afr,beta,prop,upper,lower,seed = 1999){
+generate_phenotype <- function(eur,afr,alpha,prop,upper,lower,seed = 1999){
   rate = TRUE
+  count = 1
   n_eur = nrow(eur)
   n_afr = nrow(afr)
-  linear_preds_eur = as.numeric(eur %*% beta)
-  linear_preds_afr = as.numeric(afr %*% beta)
-  sd_noise = sqrt(var(linear_preds_eur))
-  count = 1
-    while (rate) {
-    count = count + 10
-    set.seed(seed + count)
+  p = ifelse(ncol(G_eur) == ncol(G_afr), ncol(G_eur), 
+              stop("feature sizes do not match"))
+  
+  while (rate) {
+    # generate beta
+    b = sample_beta(p, alpha, seed + 10*count)
+    beta = b$beta
+    # generate phenotype
+    linear_preds_eur = as.numeric(eur %*% beta)
+    linear_preds_afr = as.numeric(afr %*% beta)
+    sd_noise = sqrt(var(linear_preds_eur))
+    set.seed(seed + 20*count)
     eps = rnorm(n_eur, mean = 0, sd = sd_noise)
     P_eur = linear_preds_eur + eps
-    q = quantile(P_eur, probs = (1 - prop))
-    P_eur <- ifelse(P_eur >= q, 1, 0)
     # now for the African
     set.seed(seed + count)
     P_afr = linear_preds_afr + rnorm(n_afr, mean = 0, sd = sd_noise)
+    q = quantile(c(P_eur,P_afr), probs = (1 - prop))
+    P_eur <- ifelse(P_eur >= q, 1, 0)
     P_afr <- ifelse(P_afr >= q, 1, 0)
-    P_afr_mean = mean(P_afr)
-    cat(paste("current proprtion of positve African penotype is ",
-              P_afr_mean,'...\n'))
-    if (P_afr_mean <= upper & P_afr_mean >= lower) {rate = FALSE}
+    P_afr_mean = mean(P_afr); P_eur_mean = mean(P_eur)
+    cat(paste("current proprtions of positve penotypes: European - ",
+              P_eur_mean,'; African - ',P_afr_mean, '...\n'))
+    if (P_afr_mean <= upper & P_afr_mean >= lower & 
+        P_eur_mean <= upper & P_eur_mean >= lower) {rate = FALSE}
+    count = count + 1
   }
-  return(list(P_eur = P_eur, P_afr = P_afr))
+  return(list(beta = beta, causal_ind = b$causal_ind,
+              P_eur = P_eur, P_afr = P_afr))
 }
 
