f4025cecef0df144a65e2732187faa3771fa9b07
Shuhan Liu <shuhan.liu.99@gmail.com>
2020-07-06 15:52:08 +0800

    add function subdata

---
+++ subdata.R
@@ -0,0 +1,26 @@
+# Function that produce a subset of data
+# @param:
+# data - full data set
+# nsize - sample size in the target subset
+# psize - feature size in the target subset
+# causal - index of causal features
+# seed - set seeds
+# return:
+# subset of data with specified dimension
+# index of causal in the subset
+
+subdata <- function(data, nsize, psize, causal, seed=1998){
+  n = nrow(data); p = ncol(data)
+  causal_prop = length(causal)/p
+  set.seed(seed + 2)
+  num = floor(causal_prop*psize)
+  causal_sub = sample(causal, size = num)
+  set.seed(seed + 4)
+  sub_non_causal = sample((1:p)[-causal], size = (psize - num))
+  sub_p = sort(c(causal_sub, sub_non_causal))
+  set.seed(seed + 8)
+  sub_n = sample(1:n, size = nsize)
+  data_sub = data[sub_n,sub_p]
+  return(list(data_sub = data_sub, causal_sub = sort(causal_sub)))
+}
+subdata(G_eur, 100, 200, causal_ind)
